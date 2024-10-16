f7058a90fce20619dc4765a8489c19219aa750df
Ashwink2000-git <ashwinkrishna1022@gmail.com>
2024-10-16 11:20:37 +0530

    Update and rename goodbye.py to script1.py

--- src/module2/goodbye.py
+++
@@ -1,3 +0,0 @@
-# src/module2/goodbye.py
-def print_goodbye():
-    print("Goodbye, World!")
---
+++ src/module2/script1.py
@@ -0,0 +1,6 @@
+# script1.py
+def subtract(a, b):
+    return a - b
+
+def module2_message():
+    return "This is Module 2!"
