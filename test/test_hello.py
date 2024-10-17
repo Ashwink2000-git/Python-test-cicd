d295d443da0db755b433435c405edb010294957e
Ashwink2000-git <ashwinkrishna1022@gmail.com>
2024-10-17 12:03:51 +0530

    Delete test/test_hello.py

--- test/test_hello.py
+++
@@ -1,9 +0,0 @@
-# tests/test_hello.py
-import io
-import sys
-from src.module1.hello import print_hello
-
-def test_print_hello(capfd):
-    print_hello()
-    captured = capfd.readouterr()
-    assert captured.out == "Hello, World!\n"
