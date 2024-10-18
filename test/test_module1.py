0392c1908f9694fa652f61f219c7eae4375a5977
Ashwink2000-git <ashwinkrishna1022@gmail.com>
2024-10-18 12:58:31 +0530

    Update test_module1.py

--- test/test_module1.py
+++ test/test_module1.py
@@ -7,11 +7,7 @@ class TestModule1(unittest.TestCase):
         self.assertEqual(script1.greet(), "Hello from Module 1!")
 
     def test_add(self):
-        if(self.assertEqual(script1.add(2, 3), 5)==1)
-            {print("The test is passing")
-            }
-        else 
-            print("the test is failed")
+       
         
 
 if __name__ == "__main__":
